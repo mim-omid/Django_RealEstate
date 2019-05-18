020d727df105a91bbfc8f35f141b9dd6009cc60d
mim-omid <mr.mim.omid@gmail.com>
2019-05-18 12:25:44 +0430

    add listings

--- btre/settings.py
+++ btre/settings.py
@@ -49,6 +49,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
